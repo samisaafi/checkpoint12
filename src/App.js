6f78b9c5f0162e4ed911a59bc3c8d046c0eb6339
samisaafi <samisaafi123@gmail.com>
2023-06-08 20:29:30 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import l1 from "./Photos/sami.jpg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CheckpointComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: {
+        fullName: 'Saafi Sami',
+        profession: 'Software Engineer',
+        imgSrc: l1,
+      },
+      show: false, // Initialize show state to false
+      startDate: new Date(),
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        startDate: new Date(),
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  showProfile = () => {
+    this.setState({ show: true });
+  };
+
+  hideProfile = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    const { fullName, profession, imgSrc } = this.state.person;
+    const { show, startDate } = this.state;
+    const timeSinceMount = Math.floor((new Date() - startDate) / 1000);
+
+    return (
+      <div>
+        {show ? (
+          <div>
+            <h1>{fullName}</h1>
+            <img src={imgSrc} alt={fullName} />
+            <h2>{profession}</h2>
+            <button onClick={this.hideProfile}>Hide Profile</button>
+          </div>
+        ) : (
+          <button onClick={this.showProfile}>Show Profile</button>
+        )}
+      
+      </div>
+    );
+  }
 }
 
-export default App;
+export default CheckpointComponent;
